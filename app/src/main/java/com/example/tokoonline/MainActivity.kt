9aec11fd09faf034f5485106e0b34a7f4d852166
kainen <kainenmi8@gmail.com>
2021-10-30 19:28:52 +0700

    First init

--- app/src/main/java/com/example/tokoonline/MainActivity.kt
+++ app/src/main/java/com/example/tokoonline/MainActivity.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layougit push -u origin maint.activity_main)
+
     }
 }
